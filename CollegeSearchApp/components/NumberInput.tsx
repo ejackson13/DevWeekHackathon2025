53b1da68d97ae66ca6511b9619f2c7aeb10e1d04
Evan Jackson <evjack1393@gmail.com>
2025-02-24 15:53:04 -0500

    start on search frontend

---
+++ CollegeSearchApp/components/NumberInput.tsx
@@ -0,0 +1,5 @@
+import { TextInput, type TextInputProps } from 'react-native';
+
+export function NumberInput({placeholder, value, onChangeText, keyboardType='number-pad', ...rest}:TextInputProps) {
+    
+}
